27ddcfe67a8037c64047a0dc131a9f13824bc790
Little coder <79461269+Ravishek51@users.noreply.github.com>
2024-10-22 18:00:27 +0530

    Update genratePdf.js

--- genratePdf.js
+++ genratePdf.js
@@ -19,52 +19,7 @@ define(['N/log','N/ui/serverWidget','N/record'],function(log,serverWidget,record
 
 
         }
-     var lineCount = curentRecord.getLineCount({
-        sublistId:'item'
-
-     });
-
-     for(var i =0;i<lineCount;i++){
-
-        var item = curentRecord.getSublistValue({
-            sublistId:'item',
-            fieldId:'item',
-            line:i
-        });
-        var amount = curentRecord.getSublistValue({
-            sublistId:'item',
-            fieldId:'amount',
-            line:i
-        });
-        var orderd = curentRecord.getSublistValue({
-            sublistId:'item',
-            fieldId:'orderpriority	',
-            line:i
-        });
-        var Qunatity = curentRecord.getSublistValue({
-            sublistId:'item',
-            fieldId:'quantity',
-            line:i
-        });
-        var Description = curentRecord.getSublistValue({
-            sublistId:'item',
-            fieldId:'description',
-            line:i
-        });
-        var price = curentRecord.getSublistValue({
-            sublistId:'item',
-            fieldId:'price',
-            line:i
-        })
-
-     }
-    //  log.debug()
-
-        log.debug("Item ",item);
-        log.debug("Amount ",amount);
-        // log.debug("Description  ",description);
-        log.debug("Code wokring Successful");
-
+    
     }
     return{
         beforeLoad:beforeLoad
